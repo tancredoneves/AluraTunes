0f6ae40801b4f9e5f98aa64bc9eac70f98f0912f
Tancredo <Tancredo@DESKTOP-F12D4HJ>
2016-12-13 01:32:48 -0200

    linq parte 1

--- AluraTunes/Program.cs
+++ AluraTunes/Program.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -14,7 +15,7 @@ namespace AluraTunes
             //criar um contexto que recebe um DbEntities
             using (var contexto = new AluraTunesEntities())
             {
-                //select simpre em um so tabela
+                //select simples em um so tabela
 
                 var query = from g in contexto.Generos
                             select g;
@@ -114,7 +115,31 @@ namespace AluraTunes
                                                   agrupado.TotalProAlbum);
                 }
 
+                //min, max e media
+                var maiorVenda = contexto.NotaFiscals.Max(nf => nf.Total);
+                var menorVenda = contexto.NotaFiscals.Min(nf => nf.Total);
+                var VendaMendia = contexto.NotaFiscals.Average(nf => nf.Total);
 
+                //transformando query (min, max e media) em um objeto
+
+                var vendas = (from nf in contexto.NotaFiscals
+                              group nf by 1 into agrupado
+                              select new
+                              {
+                                  maiorVenda = agrupado.Max(nf => nf.Total),
+                                  menorVenda = agrupado.Min(nf => nf.Total),
+                                  VendaMedia = agrupado.Average(nf => nf.Total)
+                              }).Single();
+
+                Console.WriteLine("a venda R${0}", vendas.menorVenda, vendas.menorVenda, vendas.VendaMedia);
+
+                //criando Mediana
+
+                var vendaMediana = Mediana(contexto.NotasFiscais.Select(ag => ag.Total));
+                Console.WriteLine("Venda Mediana: R$ {0}", vendaMediana);
+
+                vendaMediana = contexto.NotasFiscais.Mediana(ag => ag.Total);
+                Console.WriteLine("Venda Mediana: R$ {0}", vendaMediana);
 
 
             }
@@ -160,10 +185,37 @@ namespace AluraTunes
             
         }
 
+        public static decimal Mediana(IQueryable<decimal> origem)
+        {
+            int contagem = origem.Count();
+            var ordenado = origem.OrderBy(p => p);
 
+            var elementoCentral_1 = ordenado.Skip((contagem - 1) / 2).First();
+            var elementoCentral_2 = ordenado.Skip(contagem / 2).First();
 
+            decimal mediana = (elementoCentral_1 + elementoCentral_2) / 2;
 
+            return mediana;
+        }
+    }
+    public static class Extensions
+    {
+        public static decimal Mediana<TSource>(this IQueryable<TSource> origem, Expression<Func<TSource, decimal>> selector)
+        {
+            int contagem = origem.Count();
 
+            var funcSeletor = selector.Compile();
+            var ordenado = origem
+                .Select(selector)
+                .OrderBy(x => x);
 
+            var elementoCentral_1 = ordenado.Skip((contagem - 1) / 2).First();
+            var elementoCentral_2 = ordenado.Skip(contagem / 2).First();
+
+            decimal mediana = (elementoCentral_1 + elementoCentral_2) / 2;
+
+            return mediana;
+        }
     }
 }
+
